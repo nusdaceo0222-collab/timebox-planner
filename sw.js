38442ee9d1526eee72c1d6917d0b4d3ebd3fbd01
nusdaceo0222-collab <nusdaceo0222@gmail.com>
2026-01-14 18:34:05 +0900

    Add caching for assets in service worker
    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-self.addEventListener("fetch", () => {});
+const CACHE_NAME = "timebox-planner-v1";
+
+const ASSETS = [
+  "/timebox-planner/",
+  "/timebox-planner/index.html",
+  "/timebox-planner/manifest.webmanifest",
+  "/timebox-planner/icon-192.png",
+  "/timebox-planner/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(
+      (response) => response || fetch(event.request)
+    )
+  );
+});
